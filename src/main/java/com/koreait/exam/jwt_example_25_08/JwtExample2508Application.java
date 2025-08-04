a79e3bf792795637815db849b6f582850469d202
seongho00 <tjdwns000927@gmail.com>
2025-08-04 13:15:42 +0900

    프로젝트 세팅

---
+++ src/main/java/com/koreait/exam/jwt_example_25_08/JwtExample2508Application.java
@@ -0,0 +1,13 @@
+package com.koreait.exam.jwt_example_25_08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtExample2508Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtExample2508Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/koreait/exam/jwt_example_25_08/JwtExample2508ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreait.exam.jwt_example_25_08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtExample2508ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
